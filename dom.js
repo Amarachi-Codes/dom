a8dbe74e81db76ed1604e578d267bc32c881b02b
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-03 16:16:48 +0100

    dom.js

---
+++ dom.js
@@ -0,0 +1,8 @@
+let paragrap = document.getElementById("parag")
+// console.log(paragrap);
+
+let courses = document.getElementsByClassName("subjects")
+// console.log(courses);
+
+let sub = document.querySelectorAll("ul>ul>li")
+console.log(sub[1].textContent);
