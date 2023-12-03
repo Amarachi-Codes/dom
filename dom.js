e3bb3f26d5f51a17d539316e0598a6c870c13cf6
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-03 16:50:30 +0100

    added functionality

--- dom.js
+++ dom.js
@@ -5,4 +5,18 @@ let courses = document.getElementsByClassName("subjects")
 // console.log(courses);
 
 let sub = document.querySelectorAll("ul>ul>li")
-console.log(sub[1].textContent);
+// console.log(sub[1].textContent);
+
+let addbtn = document.querySelector(".addbg")
+
+let removebtn = document.querySelector("#removebg")
+
+addbtn.addEventListener("click",() => {
+document.body.style.backgroundColor = "grey";
+});
+
+removebtn.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "#fff"
+})
+
+
